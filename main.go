da5a2f73ec984fea1e6dca101ba845f31dc6eee8
Evan <71018479+Evan-aja@users.noreply.github.com>
2022-07-07 13:44:30 +0700

    inisialisasi kode

--- main.go
+++ main.go
@@ -1,9 +1,50 @@
 package main
 
 import (
-	"fmt"
+	"ifelse/Database"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	fmt.Println("Hello")
+	//Database
+	db := Database.Open()
+	if db != nil {
+		println("Nice, DB Connected")
+	}
+
+	// Gin Framework
+	gin.SetMode(os.Getenv("GIN_MODE"))
+	r := gin.Default()
+	r.SetTrustedProxies([]string{os.Getenv("PROXY_1"), os.Getenv("PROXY_2"), os.Getenv("PROXY_3")})
+
+	//CORS
+	r.Use(func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Controll-Allow-Methods", "POST,OPTIONS,GET,PUT,DELETE,PATCH")
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+		c.Next()
+	})
+
+	//Routers
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Welcome to IF-ELSE 2022",
+			"success": true,
+		})
+	})
+
+	//Server init
+	if err := r.Run(":5000"); err != nil {
+		log.Fatal(err.Error())
+		return
+	}
 }
