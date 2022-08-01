1cb12a3484bf5b0b41874f9a891559bac697c5e3
Evan <71018479+Evan-aja@users.noreply.github.com>
2022-08-02 03:36:00 +0700

    Registrasi, Login, dan Profil  Changes to be committed: 	modified:   .env.example 	deleted:    Auth/.gitkeep 	new file:   Auth/Auth.go 	new file:   Controller/AuthController.go 	new file:   Controller/User.go 	deleted:    Database/.gitkeep 	modified:   Database/Database.go 	new file:   Model/Register.go 	modified:   Model/Student.go 	modified:   Model/User.go 	deleted:    User/.gitkeep 	modified:   go.mod 	modified:   main.go

--- Model/User.go
+++ Model/User.go
@@ -8,10 +8,12 @@ type User struct {
 	ID            uint      `gorm:"primaryKey" json:"id"`
 	Name          string    `gorm:"notNull;size:255" json:"name"`
 	Username      string    `gorm:"size:255;notNull" json:"username"`
+	Email         string    `gorm:"size:255;notNull" json:"email"`
 	Password      string    `gorm:"notNull;size:255" json:"password"`
 	ApiToken      string    `gorm:"null;uniqueIndex;size:80;" json:"api_token"`
 	RoleId        string    `gorm:"default:1;notNull;size:255" json:"role_id"`
-	StudentId     uint32    `gorm:"null"`
+	StudentId     uint      `json:"student_id"`
+	Student       Student   `gorm:"ForeignKey:StudentId;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 	PermittedFor  string    `gorm:"default:news,tasks,quizs;size:255;notNull"`
 	RememberToken string    `gorm:"size:100"`
 	CreatedAt     time.Time `json:"created_at"`
